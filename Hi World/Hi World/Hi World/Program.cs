e63ff21650235cad3dd54c00a9f21138460f9b3c
JiriNera <veleta.j.87@gmail.com>
2021-11-08 13:54:09 +0100

    jec F to C

--- Hi World/Hi World/Hi World/Program.cs
+++ Hi World/Hi World/Hi World/Program.cs
@@ -6,54 +6,54 @@ namespace Hi_World
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Jak se jmenujes?");
-            string name = Console.ReadLine();
+            //console.writeline("jak se jmenujes?");
+            //string name = console.readline();
 
-            hello(name);
+            //hello(name);
             
-            Console.WriteLine("Zadej cele cislo " + name + ":");
+            //console.writeline("zadej cele cislo " + name + ":");
 
-            int x = int.Parse(Console.ReadLine());
+            //int x = int.parse(console.readline());
 
          
-            Console.WriteLine("Vypocet: " + Calc(x));
+            //console.writeline("vypocet: " + calc(x));
 
             double q = double.Parse(Console.ReadLine()); 
 
             Console.WriteLine(prevod(q));
         }
 
-        static void hello(string name)
-        {
-            string name5p = "";
-            if (name == "jiri")
-            {
-                name5p = "jirko";
-            }
-            else if (name == "anna")
-            {
-                name5p = "anno";
-            }
-            else if (name == "matous")
-            {
-                name5p = "matousi";
-            }
-            else
-            {
-                name5p = name + ", omlouvam se ale tve jmeno neumim sklonovat";
-            }
-            string pozdrav = "ahoj " + name5p;
-            Console.WriteLine(pozdrav);
+        //static void hello(string name)
+        //{
+        //    string name5p = "";
+        //    if (name == "jiri")
+        //    {
+        //        name5p = "jirko";
+        //    }
+        //    else if (name == "anna")
+        //    {
+        //        name5p = "anno";
+        //    }
+        //    else if (name == "matous")
+        //    {
+        //        name5p = "matousi";
+        //    }
+        //    else
+        //    {
+        //        name5p = name + ", omlouvam se ale tve jmeno neumim sklonovat";
+        //    }
+        //    string pozdrav = "ahoj " + name5p;
+        //    Console.WriteLine(pozdrav);
             
 
 
-        }
-        static int Calc(int x)
-        {
-           int result = x*2+10;
-            return result;
+        //}
+        //static int Calc(int x)
+        //{
+        //   int result = x*2+10;
+        //    return result;
            
-        }
+        //}
 
         static double prevod(double q)
         {
