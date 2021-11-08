8698d23f04093947de08a96ed0b3fb7eae5b84de
JiriNera <veleta.j.87@gmail.com>
2021-11-08 12:03:13 +0100

    vypocet

--- Hi World/Hi World/Hi World/Program.cs
+++ Hi World/Hi World/Hi World/Program.cs
@@ -5,12 +5,13 @@ namespace Hi_World
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
             Console.WriteLine("Jak se jmenujes?");
             string name = Console.ReadLine();
 
             Hello(name);
-           
+
+            Calculation z;
         }
 
         static void Hello(string name)
@@ -34,6 +35,14 @@ namespace Hi_World
             }
             string pozdrav = "Ahoj " + name5p;
             Console.WriteLine(pozdrav);
+
+
+        }
+        static void Calculation(int z)
+        {
+            int x = 5;
+            int y = x*2+10;
+            Console.WriteLine (y);
         }
     }
 }
