e2632985528470f8fbf4d23fe8d6b46630316729
JiriNera <veleta.j.87@gmail.com>
2021-11-09 12:02:59 +0100

    Invoice

---
+++ Hi World/Day2/Day2/Model/Invoice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2.Model
+{
+    class Invoice
+    {
+        public double Amount { get; set; }
+        
+        public string Customer { get; set; }
+
+        public string items { get; set; }
+    }
+}
