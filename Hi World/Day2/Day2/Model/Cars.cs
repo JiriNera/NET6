19533868fdcc83c230aa861bcc5ff393c8b2a9ad
JiriNera <veleta.j.87@gmail.com>
2021-11-09 11:54:29 +0100

    tridy

---
+++ Hi World/Day2/Day2/Model/Cars.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2.Model
+{
+    class Cars
+    {
+        public string Znacka { get; set; }
+
+        public string Barva { get; set; }
+
+        public string SPZ { get; set; }
+
+        public double KM { get; set; }
+    }
+}
---
+++ Hi World/Day2/Day2/Model/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2.Model
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public int Age()
+        {
+            DateTime today = DateTime.Today;
+            return (int)((today - Birthday).TotalDays / 365);
+        }
+    }
+}
