740b153a456875ea97dd06b1640666d7a32f672d
JiriNera <veleta.j.87@gmail.com>
2021-11-10 10:08:10 +0100

    2d point

---
+++ Hi World/Day3/Model/Point2d.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3.Model
+{
+    class Point2d
+    {
+        
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point2d(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int Area()
+        {
+            return X * Y;
+        }
+
+        public override string ToString()
+        {
+            return $"Souřadnice bodu jsou [{X},{Y}], plocha od [0,0] k zadanému bodu je {Area()} jednotek^2";
+        }
+    }
+}
+    
+
---
+++ Hi World/Day3/Program.cs
@@ -0,0 +1,16 @@
+﻿using Day3.Model;
+using System;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point2d bod1 = new Point2d(10, 5);
+            Point2d bod2 = new Point2d(3, 6);
+            Console.WriteLine(bod1);
+            Console.WriteLine(bod2);
+        }
+    }
+}
