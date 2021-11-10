dd65aec8415cf14377045cd9c3cb5a3b98936477
JiriNera <veleta.j.87@gmail.com>
2021-11-10 10:43:39 +0100

    pointmath - soucet

---
+++ Hi World/Day3/Function/PointMath.cs
@@ -0,0 +1,20 @@
+﻿using Day3.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3.Function
+{
+    class PointMath
+    {
+        public static Point2d AddPoints(Point2d p1,Point2d p2)
+        {
+            int x = p1.X + p2.X;
+            int y = p1.Y + p2.Y;
+
+            return new Point2d(x, y);
+        }
+    }
+}
--- Hi World/Day3/Program.cs
+++ Hi World/Day3/Program.cs
@@ -1,4 +1,5 @@
-﻿using Day3.Model;
+﻿using Day3.Function;
+using Day3.Model;
 using System;
 
 namespace Day3
@@ -7,8 +8,16 @@ namespace Day3
     {
         static void Main(string[] args)
         {
-            Point2d bod1 = new Point2d(7, 9);
-            Console.WriteLine(bod1);
+            Point2d pointA = new Point2d(7, 9);
+            Point2d pointB = new Point2d(7, 9);
+            Console.WriteLine(pointA);
+            Console.WriteLine(pointB);
+
+            var soucet = PointMath.AddPoints(pointA, pointB);
+            Console.WriteLine("Soucet: " + soucet);
+
+
+
         }
     }
 }
